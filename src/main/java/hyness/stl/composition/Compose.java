fe68c89174303438d6d16a81ccf0b8f4d5fde811
Curtis Madsen <culnaur@gmail.com>
2016-08-01 20:02:40 -0400

    Started adding a method for concatonate.

--- src/main/java/hyness/stl/composition/Compose.java
+++ src/main/java/hyness/stl/composition/Compose.java
@@ -12,6 +12,8 @@ import hyness.stl.Pair;
 import hyness.stl.TreeNode;
 import hyness.stl.grammar.flat.STLflat;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Vector;
 
 /**
@@ -72,5 +74,47 @@ public class Compose {
             stl.limitsMap = limitsMap;
             return stl;
         }
+        
+        public static STLflat composeWithConcatonate(STLflat left, STLflat right, Map<String, List<String>> internalMapping) {
+            //TODO:  Utilize internal mapping
+            int i = 1;
+            HashMap<Pair<String, Boolean>,String> m1Map = new HashMap<Pair<String, Boolean>,String>();
+            HashMap<Pair<String, Boolean>,String> ioMap = new HashMap<Pair<String, Boolean>,String>();
+            HashMap<String,HashMap<String,Double>> limitsMap = new HashMap<String,HashMap<String,Double>>();
+            Vector<String> leftPorts = new Vector<String>();
+            leftPorts.add("phi1");
+            for (Pair<String, Boolean> pair : left.maps.get("io").keySet()) {
+                leftPorts.add(left.maps.get("io").get(pair));
+		m1Map.put(new Pair<String, Boolean>(left.maps.get("io").get(pair), true), "io" + i);
+		ioMap.put(new Pair<String, Boolean>("io" + i, null), "io" + i);
+		HashMap<String,Double> limits = new HashMap<String,Double>();
+		limits.put("min", left.limitsMap.get(left.maps.get("io").get(pair)).get("min"));
+		limits.put("max", left.limitsMap.get(left.maps.get("io").get(pair)).get("max"));
+		limitsMap.put("io" + i, limits);
+		i++;
+            }
+            Vector<String> rightPorts = new Vector<String>();
+            rightPorts.add("phi2");
+            for (Pair<String, Boolean> pair : right.maps.get("io").keySet()) {
+                rightPorts.add(right.maps.get("io").get(pair));
+		m1Map.put(new Pair<String, Boolean>(right.maps.get("io").get(pair), false), "io" + i);
+		ioMap.put(new Pair<String, Boolean>("io" + i, null), "io" + i);
+		HashMap<String,Double> limits = new HashMap<String,Double>();
+		limits.put("min", right.limitsMap.get(right.maps.get("io").get(pair)).get("min"));
+		limits.put("max", right.limitsMap.get(right.maps.get("io").get(pair)).get("max"));
+		limitsMap.put("io" + i, limits);
+		i++;
+            }
+            TreeNode leftNode = new ModuleLeaf("phi1", leftPorts);
+            TreeNode rightNode = new ModuleLeaf("phi2", rightPorts);
+            TreeNode operationNode = new ModuleNode(Operation.CONCAT, leftNode, rightNode, "m1");
+            STLflat stl = new STLflat(operationNode);
+            stl.modules.put("phi1", left);
+            stl.modules.put("phi2", right);
+            stl.maps.put("m1", m1Map);
+            stl.maps.put("io", ioMap);
+            stl.limitsMap = limitsMap;
+            return stl;
+	}
     
 }
